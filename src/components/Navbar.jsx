de6540170c2cfe7a7c17a85ce4f12be3c34d3ef9
DanielGallegosLB <daniel.gallegoslb@gmail.com>
2024-08-19 13:37:30 -0300

    Landing page v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,30 +6,32 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   const handleNav = () => {
-    console.log(nav);
-    console.log('click');
     setNav(!nav);
   }
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] px-4 text-white">
-
-      <div className='' onClick={handleNav}>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] px-4  text-white">      
+      <h1 className='w-full text-3xl font-bold text-[#000000] m-4'>Inscripción Practica</h1>
+        <ul className='hidden md:flex'>
+          <li className='p-4'>Inicio</li>
+          <li className='p-4'>Compañia</li>
+          <li className='p-4'>Recursos</li>
+          <li className='p-4'>Acerca</li>
+          <li className='p-4'>Contacto</li>
+        </ul>
+      <div  onClick={handleNav} className='block md:hidden'>
         {!nav ? <AiOutlineClose  size={30}/> : <AiOutlineMenu size={30}/>}
       </div>
-      
-      <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900' >
+      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-100 bg-[#ff6200] ease-in-out duration-500' : 'fixed top-0 left-[-100%] ease-in-out duration-500'} >
         
-        <h1 className='w-full text-3xl font-bold text-[#000000] m-4'>Inscripción Practica</h1>
-        
-
         <ul className='uppercase p-4'>
-        <li className='p-4 border-b border-gray-600'>Inicio</li>
-        <li className='p-4 border-b border-gray-600'>Compañia</li>
-        <li className='p-4 border-b border-gray-600'>Recursos</li>
-        <li className='p-4 border-b border-gray-600'>Acerca</li>
-        <li className='p-4'>Contacto</li>
+          <li className='p-4 border-b border-white-100'>Inicio</li>
+          <li className='p-4 border-b border-white-100'>Compañia</li>
+          <li className='p-4 border-b border-white-100'>Recursos</li>
+          <li className='p-4 border-b border-white-100'>Acerca</li>
+          <li className='p-4'>Contacto</li>
         </ul>
       </div>
+      
     </div>
   );
 }
